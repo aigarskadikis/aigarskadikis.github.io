6cb60d85a9fde93a1893933671972dd8afc29f8b
aigarskadikis <aigars.kadikis@gmail.com>
2021-01-30 22:47:52 +0200

    next

--- check.sh
+++ check.sh
@@ -15,7 +15,6 @@ AGO5D=$((NOW-432000))
 AGO7D=$((NOW-604800))
 AGO28D=$((NOW-2419200))
 
-
 echo $1 = DB name
 echo $2 = DB engine
 echo
@@ -45,87 +44,27 @@ fi
 # if second argument is explicitly mysql then ignore postgres
 [ "$2" == "mysql" ] && POSTGRES=0
 
-# test mysql connection
-if [ "$MYSQL" -eq "1" ];then
-mysql $DBNAME -e "SELECT VERSION()" > /dev/null 2>&1
+[ "$MYSQL" -eq "1" ] && SQL_CLIENT="mysql $DBNAME -B -N -e"
+
+[ "$POSTGRES" -eq "1" ] && SQL_CLIENT="psql $DBNAME -t -c"
+
+# test database connection
+$SQL_CLIENT "
+SELECT VERSION();
+" > /dev/null 2>&1
 exit_code=$?
 if [ $exit_code -ne 0 ]; then
 echo $exit_code. cannot connect to DB engine or database does not exist
 exit $exit_code
-fi
 fi
 
 echo POSTGRES=$POSTGRES
 echo MYSQL=$MYSQL
 echo
 
-# test postgres connection
-[ "$POSTGRES" -eq "1" ] && psql $DBNAME -t -c 'SELECT VERSION()' 
-
 echo ######## VERSION OF ZABBIX DATABASE ########
-[ "$MYSQL" -eq "1" ] && DBVESRION=$(mysql $DBNAME -B -N -e "
+$SQL_CLIENT "
 SELECT mandatory FROM dbversion;
-")
-[ "$POSTGRES" -eq "1" ] && DBVESRION=$(psql $DBNAME -t -c "
-SELECT mandatory FROM dbversion;
-")
-echo "$DBVESRION = version of zabbix database"
-
-echo EVENTS
-[ "$MYSQL" -eq "1" ] && EVENTS=$(mysql $DBNAME -e "
-SELECT source,COUNT(*) FROM events GROUP BY source;
-")
-[ "$POSTGRES" -eq "1" ] && EVENTS=$(psql $DBNAME -c "
-SELECT source,COUNT(*) FROM events GROUP BY source;
-")
-echo "$EVENTS"
-
-echo EVENTS IN LAST 1 HOUR
-[ "$MYSQL" -eq "1" ] && EVENTS=$(mysql $DBNAME -e "
-SELECT COUNT(*) FROM events WHERE clock > UNIX_TIMESTAMP(NOW()-INTERVAL 1 HOUR);
-")
-[ "$POSTGRES" -eq "1" ] && EVENTS=$(psql $DBNAME -c "
-SELECT COUNT(*) FROM events WHERE clock > EXTRACT(EPOCH FROM (NOW() - INTERVAL '1 HOUR'));
-")
-echo "$EVENTS"
-
-echo EVENTS IN LAST 1 HOUR
-[ "$MYSQL" -eq "1" ] && SESSIONS=$(mysql $DBNAME -B -N -e "
-SELECT COUNT(*) FROM sessions;
-")
-[ "$POSTGRES" -eq "1" ] && SESSIONS=$(psql $DBNAME -t -c "
-SELECT COUNT(*) FROM sessions;
-")
-echo "$SESSIONS"
-
-
-
-echo ALERTS IN LAST 8 HOURS
-[ "$MYSQL" -eq "1" ] && ALERTS=$(mysql $DBNAME -e "
-SELECT COUNT(*),
-CASE alerts.status
-WHEN 0 THEN 'NOT_SENT'
-WHEN 1 THEN 'SENT'
-WHEN 2 THEN 'FAILED'
-WHEN 3 THEN 'NEW'
-END AS status
-FROM alerts
-WHERE alerts.clock > UNIX_TIMESTAMP (NOW()-INTERVAL 8 HOUR)
-GROUP BY alerts.status;
-")
-[ "$POSTGRES" -eq "1" ] && ALERTS=$(psql $DBNAME -t -c "
-SELECT COUNT(*),
-CASE alerts.status
-WHEN 0 THEN 'NOT_SENT'
-WHEN 1 THEN 'SENT'
-WHEN 2 THEN 'FAILED'
-WHEN 3 THEN 'NEW'
-END AS status
-FROM alerts
-WHERE alerts.clock > EXTRACT(EPOCH FROM (NOW() - INTERVAL '8 HOUR'))
-GROUP BY alerts.status;
-")
-echo "$ALERTS"
-
+"
 
 echo
