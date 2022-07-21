714cfe3bbdf9bdb390423b299f8d2538e505477c
catonrug <aigars.kadikis@gmail.com>
2022-07-21 22:20:53 +0300

    next

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,11 +18,11 @@
 # -- Project information -----------------------------------------------------
 
 project = 'Aigars Kadikis'
-copyright = '2021, Aigars'
+copyright = '2022, Aigars'
 author = 'Aigars'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.2'
 
 
 # -- General configuration ---------------------------------------------------
