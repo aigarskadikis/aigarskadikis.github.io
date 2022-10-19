27a5294084a713b559d4cee30b30d9d858c5c18b
catonrug <aigars.kadikis@gmail.com>
2022-10-19 23:20:44 +0300

    next

--- src/server.sql
+++ src/server.sql
@@ -99,31 +99,35 @@ WHERE hosts.status=0
 AND interface.main=1;
 
 --items in use
-SELECT CASE items.type
-WHEN 0 THEN 'Zabbix agent'
-WHEN 2 THEN 'Zabbix trapper'
-WHEN 3 THEN 'Simple check'
-WHEN 5 THEN 'Zabbix internal'
-WHEN 7 THEN 'Zabbix agent (active) check'
-WHEN 8 THEN 'Aggregate'
-WHEN 9 THEN 'HTTP test (web monitoring scenario step)'
-WHEN 10 THEN 'External check'
-WHEN 11 THEN 'Database monitor'
-WHEN 12 THEN 'IPMI agent'
-WHEN 13 THEN 'SSH agent'
-WHEN 14 THEN 'TELNET agent'
-WHEN 15 THEN 'Calculated'
-WHEN 16 THEN 'JMX agent'
-WHEN 17 THEN 'SNMP trap'
-WHEN 18 THEN 'Dependent item'
-WHEN 19 THEN 'HTTP agent'
-WHEN 20 THEN 'SNMP agent'
-END as type,COUNT(*)
-FROM items
-JOIN hosts ON (hosts.hostid=items.hostid)
-WHERE hosts.status=0
-AND items.status=0
-GROUP BY items.type
+SELECT CASE items.type 
+WHEN 0 THEN 'Zabbix agent' 
+WHEN 1 THEN 'SNMPv1 agent' 
+WHEN 2 THEN 'Zabbix trapper' 
+WHEN 3 THEN 'Simple check' 
+WHEN 4 THEN 'SNMPv2 agent' 
+WHEN 5 THEN 'Zabbix internal' 
+WHEN 6 THEN 'SNMPv3 agent' 
+WHEN 7 THEN 'Zabbix agent (active) check' 
+WHEN 8 THEN 'Aggregate' 
+WHEN 9 THEN 'HTTP test (web monitoring scenario step)' 
+WHEN 10 THEN 'External check' 
+WHEN 11 THEN 'Database monitor' 
+WHEN 12 THEN 'IPMI agent' 
+WHEN 13 THEN 'SSH agent' 
+WHEN 14 THEN 'TELNET agent' 
+WHEN 15 THEN 'Calculated' 
+WHEN 16 THEN 'JMX agent' 
+WHEN 17 THEN 'SNMP trap' 
+WHEN 18 THEN 'Dependent item' 
+WHEN 19 THEN 'HTTP agent' 
+WHEN 20 THEN 'SNMP agent' 
+WHEN 21 THEN 'Script item' 
+END as type,COUNT(*) 
+FROM items 
+JOIN hosts ON (hosts.hostid=items.hostid) 
+WHERE hosts.status=0 
+AND items.status=0 
+GROUP BY items.type 
 ORDER BY COUNT(*) DESC;
 
 --exceptions in update interval. when a user install a custom update frequency in a host level and the frequency differs from template level. This query also detects difference between nested templates
