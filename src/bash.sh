1ce55bb62cf65211d69590248917005721c4b716
catonrug <aigars.kadikis@gmail.com>
2023-01-02 14:41:50 +0200

    next

--- src/bash.sh
+++ src/bash.sh
@@ -13,3 +13,8 @@ sed 's/^[\t ]*//g;s/[\t ]*$//g'
 # endless loop to deliver metric
 while true; do zabbix_sender  -z 127.0.0.1 -s $(hostname) -k agent.ping -o 1; sleep 30; done
 
+# test disk throughput
+dd if=/dev/urandom of=/db/mount/point/512M bs=1M count=512 oflag=direct
+dd if=/dev/urandom of=/db/mount/point/5GB bs=1M count=5120 oflag=direct
+dd if=/dev/urandom of=/db/mount/point/50GB bs=1M count=51200 oflag=direct
+
