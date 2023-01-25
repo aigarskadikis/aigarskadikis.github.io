4d0ccb0930acf4912e0ecbbb0cfdb103852e673d
catonrug <aigars.kadikis@gmail.com>
2023-01-25 23:45:34 +0200

    next

--- src/delete.sql
+++ src/delete.sql
@@ -15,3 +15,11 @@ LEFT JOIN event_recovery ON (event_recovery.eventid=e.eventid)
 WHERE event_recovery.c_eventid IS NOT NULL
 AND e.clock < 1234;
 
+--delete dublicate values in table, keep the newest one. this is NOT discard and unchanged!
+DELETE t1 FROM history_text t1
+INNER JOIN history_text t2
+WHERE t1.itemid=382198 AND
+t1.clock < t2.clock AND
+t1.value = t2.value AND
+t1.itemid = t2.itemid;
+
