8bff5e0017a6334be10b03c5e494e23645012563
catonrug <aigars.kadikis@gmail.com>
2023-01-29 13:02:23 +0200

    next

--- src/bash.sh
+++ src/bash.sh
@@ -77,3 +77,77 @@ t1.itemid=$ITEMID;
 " | mysql zabbix
 } done
 
+# discard unchanged 'history_text' for all item IDs
+mysql \
+--database='zabbix' \
+--silent \
+--skip-column-names \
+--batch \
+--execute="
+SELECT items.itemid
+FROM items, hosts
+WHERE hosts.hostid=items.hostid
+AND hosts.status IN (0,1)
+AND items.value_type=4
+AND items.flags IN (0,4)
+" | \
+while IFS= read -r ITEMID
+do {
+echo $ITEMID
+sleep 0.01
+echo "
+DELETE FROM history_text WHERE itemid=$ITEMID AND clock IN (
+SELECT clock from (
+SELECT clock, value, r, v2 FROM (
+SELECT clock, value, LEAD(value,1) OVER (order by clock) AS v2,
+CASE
+WHEN value <> LEAD(value,1) OVER (order by clock)
+THEN value
+ELSE 'zero'
+END AS r
+FROM history_text WHERE itemid=$ITEMID
+) x2
+where r = 'zero'
+) x3
+WHERE v2 IS NOT NULL
+)
+" | mysql zabbix
+} done
+
+# discard unchanged 'history_str' for all item IDs
+mysql \
+--database='zabbix' \
+--silent \
+--skip-column-names \
+--batch \
+--execute="
+SELECT items.itemid
+FROM items, hosts
+WHERE hosts.hostid=items.hostid
+AND hosts.status IN (0,1)
+AND items.value_type=1
+AND items.flags IN (0,4)
+" | \
+while IFS= read -r ITEMID
+do {
+echo $ITEMID
+sleep 0.01
+echo "
+DELETE FROM history_str WHERE itemid=$ITEMID AND clock IN (
+SELECT clock from (
+SELECT clock, value, r, v2 FROM (
+SELECT clock, value, LEAD(value,1) OVER (order by clock) AS v2,
+CASE
+WHEN value <> LEAD(value,1) OVER (order by clock)
+THEN value
+ELSE 'zero'
+END AS r
+FROM history_str WHERE itemid=$ITEMID
+) x2
+where r = 'zero'
+) x3
+WHERE v2 IS NOT NULL
+)
+" | mysql zabbix
+} done
+
