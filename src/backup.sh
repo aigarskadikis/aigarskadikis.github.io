bcbf1c04dc168b7bf0d0978abe858f84aec55aea
aigarskadikis <aigars.kadikis@gmail.com>
2023-02-23 21:15:01 +0200

    mariadb backup

--- src/backup.sh
+++ src/backup.sh
@@ -81,6 +81,57 @@ mv "$BACKUP_DIR/quick.restore.sql.gz" "$BACKUP_DIR/quick.restore.$DATE.sql.gz"
 find /backup -type f -mtime +30
 find /backup -type f -mtime +30 -delete
 
+# mariadb backup
+BACKUP_DIR=/backup
+KEEP_DAYS=90
+DBNAME=zabbix
+DATE=$(date +%Y%m%d.%H%M)
+mkdir -p "$BACKUP_DIR"
+echo "schema"
+mysqldump \
+--defaults-file=~/zabbix_backup.cnf \
+--flush-logs \
+--single-transaction \
+--create-options \
+--no-data \
+$DBNAME | gzip --fast > "$BACKUP_DIR/schema.sql.gz" && \
+echo "data" && \
+mysqldump \
+--defaults-file=~/zabbix_backup.cnf \
+--flush-logs \
+--single-transaction \
+--no-tablespaces \
+--ignore-table=$DBNAME.history \
+--ignore-table=$DBNAME.history_log \
+--ignore-table=$DBNAME.history_str \
+--ignore-table=$DBNAME.history_text \
+--ignore-table=$DBNAME.history_uint \
+--ignore-table=$DBNAME.trends \
+--ignore-table=$DBNAME.trends_uint \
+$DBNAME > "$BACKUP_DIR/data.sql" && \
+echo "compressing data" && gzip --fast "$BACKUP_DIR/data.sql" && \
+echo "snapshot" && mysqldump \
+--defaults-file=~/zabbix_backup.cnf \
+--flush-logs \
+--single-transaction \
+--ignore-table=$DBNAME.history \
+--ignore-table=$DBNAME.history_log \
+--ignore-table=$DBNAME.history_str \
+--ignore-table=$DBNAME.history_text \
+--ignore-table=$DBNAME.history_uint \
+--ignore-table=$DBNAME.trends \
+--ignore-table=$DBNAME.trends_uint \
+$DBNAME > "$BACKUP_DIR/snapshot.sql" && \
+echo "compressing snapshot" && gzip --fast "$BACKUP_DIR/snapshot.sql" && \
+mv "$BACKUP_DIR/schema.sql.gz" "$BACKUP_DIR/schema.$DATE.sql.gz" && \
+mv "$BACKUP_DIR/data.sql.gz" "$BACKUP_DIR/data.$DATE.sql.gz" && \
+mv "$BACKUP_DIR/snapshot.sql.gz" "$BACKUP_DIR/snapshot.$DATE.sql.gz"
+if [ -d "$BACKUP_DIR" ]; then
+echo "deleting files older than $KEEP_DAYS days:"
+find "$BACKUP_DIR" -maxdepth 1 -type f -name '*.gz' -mtime +$KEEP_DAYS
+find "$BACKUP_DIR" -maxdepth 1 -type f -name '*.gz' -mtime +$KEEP_DAYS -delete
+fi
+
 # rotate backups for 30 days
 find /backup -type f -mtime +30
 find /backup -type f -mtime +30 -delete
