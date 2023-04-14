e95163c15b3350447abef62b5d12c54e59282d26
catonrug <aigars.kadikis@gmail.com>
2023-04-14 13:24:43 +0300

    next

---
+++ src/ps.cmd
@@ -0,0 +1,2 @@
+PowerShell.exe -nologo -command "Get-Process" > PowerShell.Get.Process.plain.txt
+PowerShell.exe -nologo -command "Get-Process | ConvertTo-JSON" > c:\temp\PowerShell.Get.Process.json
