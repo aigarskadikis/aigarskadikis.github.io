438e30f98973faad3d064eb6b667e8990cfc5d19
catonrug <aigars.kadikis@gmail.com>
2023-09-21 21:35:18 +0300

    next

--- src/part.sql
+++ src/part.sql
@@ -43,3 +43,56 @@ RENAME TABLE history_str TO history_str_tmp; RENAME TABLE history_str_old TO his
 RENAME TABLE history_text TO history_text_tmp; RENAME TABLE history_text_old TO history_text; INSERT IGNORE INTO history_text SELECT * FROM history_text_tmp;
 RENAME TABLE history_log TO history_log_tmp; RENAME TABLE history_log_old TO history_log; INSERT IGNORE INTO history_log SELECT * FROM history_log_tmp;
 
+--validate if there are 10 new tables
+SHOW CREATE TABLE history_new\G
+SHOW CREATE TABLE history_uint_new\G
+SHOW CREATE TABLE history_str_new\G
+SHOW CREATE TABLE history_log_new\G
+SHOW CREATE TABLE history_text_new\G
+SHOW CREATE TABLE history_part\G
+SHOW CREATE TABLE history_uint_part\G
+SHOW CREATE TABLE history_str_part\G
+SHOW CREATE TABLE history_log_part\G
+SHOW CREATE TABLE history_text_part\G
+
+--detach the current "history" tables from the application and set new/empty tables in place
+RENAME TABLE history_uint TO history_uint_old; RENAME TABLE history_uint_new TO history_uint;
+RENAME TABLE history TO history_old; RENAME TABLE history_new TO history;
+RENAME TABLE history_str TO history_str_old; RENAME TABLE history_str_new TO history_str;
+RENAME TABLE history_text TO history_text_old; RENAME TABLE history_text_new TO history_text;
+RENAME TABLE history_log TO history_log_old; RENAME TABLE history_log_new TO history_log;
+
+--open "tmux" and migrate the data from an unpartitioned table space to a partitioned table space
+SET SESSION SQL_LOG_BIN=0;
+INSERT IGNORE INTO history_str_part SELECT * FROM history_str_old;
+INSERT IGNORE INTO history_text_part SELECT * FROM history_text_old;
+INSERT IGNORE INTO history_log_part SELECT * FROM history_log_old;
+INSERT IGNORE INTO history_uint_part SELECT * FROM history_uint_old;
+INSERT IGNORE INTO history_part SELECT * FROM history_old;
+
+--attach/swap the partitioned tables to the application later
+RENAME TABLE history_uint TO history_uint_tmp; RENAME TABLE history_uint_part TO history_uint;
+RENAME TABLE history TO history_tmp; RENAME TABLE history_part TO history;
+RENAME TABLE history_str TO history_str_tmp; RENAME TABLE history_str_part TO history_str;
+RENAME TABLE history_text TO history_text_tmp; RENAME TABLE history_text_part TO history_text;
+RENAME TABLE history_log TO history_log_tmp; RENAME TABLE history_log_part TO history_log;
+
+--transfer back data which was collected during the migration
+INSERT IGNORE INTO history_uint SELECT * FROM history_uint_tmp;
+INSERT IGNORE INTO history SELECT * FROM history_tmp;
+INSERT IGNORE INTO history_str SELECT * FROM history_str_tmp;
+INSERT IGNORE INTO history_text SELECT * FROM history_text_tmp;
+INSERT IGNORE INTO history_log SELECT * FROM history_log_tmp;
+
+--drop unnecessary tables
+DROP TABLE history_uint_tmp;
+DROP TABLE history_tmp;
+DROP TABLE history_str_tmp;
+DROP TABLE history_text_tmp;
+DROP TABLE history_log_tmp;
+DROP TABLE history_str_old;
+DROP TABLE history_text_old;
+DROP TABLE history_log_old;
+DROP TABLE history_uint_old;
+DROP TABLE history_old;
+
