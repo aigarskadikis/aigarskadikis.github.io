7c5a70cdf4b2e9fc53cd4208c33092836ee973c1
catonrug <aigars.kadikis@gmail.com>
2023-10-31 16:30:39 +0200

    next

---
+++ src/psql.sql
@@ -0,0 +1,19 @@
+--long running queries. postgres. PostgreSQL
+SELECT
+pid,
+user,
+pg_stat_activity.query_start,
+NOW() - pg_stat_activity.query_start AS query_time,
+query,
+state,
+wait_event_type,
+wait_event
+FROM pg_stat_activity
+WHERE (NOW() - pg_stat_activity.query_start) > interval '3 seconds';
+
+--monitor process. postgres. PostgreSQL
+SELECT * FROM pg_stat_activity WHERE state != 'idle';
+
+--what is minor version of Zabbix
+SELECT * FROM dbversion;
+
