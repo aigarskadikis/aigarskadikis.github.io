17dc118d886a34bdf78f82c11aaa11f3bcf530d2
catonrug <aigars.kadikis@gmail.com>
2025-02-07 11:52:35 +0200

    next

--- src/my.sh
+++ src/my.sh
@@ -22,6 +22,17 @@ lsof -p $(pidof mysqld) > /tmp/mysqld.list.open.files.txt
 du -lah /var/lib/mysql/zabbix | grep "$(date --date='2 days ago' '+p%Y_%m_%d')"
 du -lah /var/lib/mysql/zabbix | grep "$(date --date='2 days ago' '+p%Y%m%d0000')"
 
+# extract legacy SNMP agent items in use
+mysql --host=127.0.0.1 --user=root --password='zabbix' --database=zabbix --execute="
+SELECT DISTINCT templates.name, items.flags, items.name, items.snmp_oid FROM hosts templates, hosts_templates, items
+WHERE templates.hostid=items.hostid AND templates.hostid=hosts_templates.templateid AND hosts_templates.hostid IN (
+SELECT hostid FROM hosts WHERE status=0
+)
+AND items.type=20
+AND templates.status=3
+ORDER BY 1,2,3;
+" > /tmp/all.snmp.templates.in.use.with.legacy.snmp.agent.item.tsv
+
 # Track progress of "OPTIMIZE TABLE" in MySQL
 watch -n1 "ls -Rltr /var/lib/mysql/zabbix | grep '#sql'"
 
