af31159ad3d259531dd884a795a5c5075d21e602
catonrug <aigars.kadikis@gmail.com>
2025-02-23 10:45:12 +0200

    next

--- src/pod.sh
+++ src/pod.sh
@@ -12,3 +12,14 @@ podman run --name pg17ts2181 -t \
 -p 7417:5432 \
 -d timescale/timescaledb:2.18.1-pg17
 
+# Pod podman unqualified-search. did not resolve to an alias and no unqualified-search registries are defined in "/etc/containers/registries.conf"
+echo 'unqualified-search-registries = ["docker.io"]' | sudo tee /etc/containers/registries.conf
+
+# reinstall version of zabbix monitoring proxy
+sed -i 's|alpine-7.0.*$|alpine-7.0.9|g' values.yaml
+helm uninstall my-release && helm install my-release -f values.yaml ./
+kubectl get secret zabbix-service-account -n default -o jsonpath={.data.token} | base64 -d
+
+# restart pod (by deleting it)
+kubectl get pods | grep -Eo "zabbix-proxy\S+" | xargs kubectl delete pod
+
