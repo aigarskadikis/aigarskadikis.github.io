8bbf812125ae372a2c00d774cae36e12100a0f4a
catonrug <aigars.kadikis@gmail.com>
2025-03-08 21:36:41 +0200

    next

---
+++ ducktape/fill-missing-values.js
@@ -0,0 +1,35 @@
+function fillEmptyValues(arr) {
+    var allKeys = {};
+    var i, j, obj, key;
+
+    // Collect all keys
+    for (i = 0; i < arr.length; i++) {
+        obj = arr[i];
+        for (key in obj) {
+            allKeys[key] = true;
+        }
+    }
+
+    // Convert keys object to an array
+    var keys = [];
+    for (key in allKeys) {
+        keys.push(key);
+    }
+
+    // Fill missing values
+    var result = [];
+    for (i = 0; i < arr.length; i++) {
+        var newObj = {};
+        for (j = 0; j < keys.length; j++) {
+            key = keys[j];
+            newObj[key] = arr[i][key] || "";
+        }
+        result.push(newObj);
+    }
+
+    return result;
+}
+
+var output = fillEmptyValues(JSON.parse(value));
+return (JSON.stringify(output));
+
